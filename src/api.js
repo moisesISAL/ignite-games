6a8b248cfad9d0b6912125768b7f96b9c61a5469
moisesISAL <moises@isaldigital.com>
2021-01-04 15:03:09 -0600

    1. fetch Game detail - 01

--- src/api.js
+++ src/api.js
@@ -38,5 +38,8 @@ const new_games = `games?dates=${lastYear},${currentDate}&ordering=-released&pag
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
 export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
 export const newGamesUrl = () => `${base_url}${new_games}`;
-
+//Game details
+export const gameDetailsUrl =  (game_id)=>`${base_url}games/${game_id}` 
+//game screenshots
+export const gameScreenshotUrl =  (game_id)=>`${base_url}games/${game_id}/screenshots` 
 // getCurrentMonth(); //to run this you need to use node js  in terminal
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 //Styling and animation
 import styled from 'styled-components'
 import {motion} from 'framer-motion';
+import {useDispatch} from 'react-redux';
+import {loadDetail} from '../actions/detailAction'
 
-const Game = ({name,released,image})=>{
+const Game = ({name,released,image, id})=>{
+    //load detail
+    const dispatch = useDispatch();
+    const loadDetailHandler = () => {
+        dispatch(loadDetail(id));
+    }
     return(
-        <StyledGame>
+        <StyledGame onClick={loadDetailHandler}>
             <h3>{name}</h3>
             <p>{released}</p>
             <img src={image} alt={name}/>
