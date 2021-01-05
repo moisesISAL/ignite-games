17821e3390892a94292387aa24f8c0b442c44ec7
moisesISAL <moises@isaldigital.com>
2021-01-05 13:03:49 -0600

    18. Adding icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -2,53 +2,86 @@ import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
-import {useHistory} from 'react-router-dom';
-import {smallImage} from '../util'
+import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
+//images
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
-const GameDetail = ({pathId}) => {
+const GameDetail = ({ pathId }) => {
   const history = useHistory();
-const exitDetailHandler = (e) => {
-  const element = e.target;
-  if(element.classList.contains("shadow")){
-    document.body.style.overflow="auto";
-    history.push("/");
-  }
-}
+
+  const exitDetailHandler = (e) => {
+    const element = e.target;
+    if (element.classList.contains("shadow")) {
+      document.body.style.overflow = "auto";
+      history.push("/");
+    }
+  };
+  //get platform images
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
 
   const { screen, game, isLoading } = useSelector((state) => state.detail);
   return (
     <>
-    {!isLoading && (
-    <CardShadow className='shadow' onClick={exitDetailHandler}>
-      <Detail layoutId={pathId}>
-        <Stats>
-          <div className="rating">
-            <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
-            <p>Rating: {game.rating}</p>
-          </div>
-          <Info>
-            <h3>Platforms</h3>
-            <Platforms>
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+      {!isLoading && (
+        <CardShadow className="shadow" onClick={exitDetailHandler}>
+          <Detail layoutId={pathId}>
+            <Stats>
+              <div className="rating">
+                <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
+                <p>Rating: {game.rating}</p>
+              </div>
+              <Info>
+                <h3>Platforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <img key={data.platform.id} alt='namexd' src={getPlatform(data.platform.name)}/>
+                  ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media>
+              <motion.img
+                layoutId={`image ${pathId}`}
+                src={smallImage(game.background_image, 1280)}
+                alt="name"
+              />
+            </Media>
+            <Description>
+              <p>{game.description_raw}</p>
+            </Description>
+            <div className="gallery">
+              {screen.results.map((screen) => (
+                <img
+                  key={screen.id}
+                  src={smallImage(screen.image, 640)}
+                  alt="name"
+                />
               ))}
-            </Platforms>
-          </Info>
-        </Stats>
-        <Media>
-          <motion.img layoutId={`image ${pathId}`} src={smallImage(game.background_image, 1280)} alt="name" />
-        </Media>
-        <Description>
-          <p>{game.description_raw}</p>
-        </Description>
-        <div className="gallery">
-          {screen.results.map((screen) => (
-            <img key={screen.id} src={smallImage(screen.image, 640)} alt="name" />
-          ))}
-        </div>
-      </Detail>
-    </CardShadow>
-    )}
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
     </>
   );
 };
@@ -57,30 +90,31 @@ const CardShadow = styled(motion.div)`
   width: 100%;
   min-height: 100vh;
   overflow-y: scroll;
-  background: rgba(0,0,0, .5);
+  background: rgba(0, 0, 0, 0.5);
   position: fixed;
   top: 0;
   left: 0;
-  &::-webkit-scrollbar{
-    width:0.5rem;
+  z-index: 5;
+  &::-webkit-scrollbar {
+    width: 0.5rem;
   }
-  &::-webkit-scrollbar-thumb{
+  &::-webkit-scrollbar-thumb {
     background-color: #ff7676;
   }
-  &::-webkit-scrollbar-track{
+  &::-webkit-scrollbar-track {
     background: white;
   }
 `;
 
 const Detail = styled(motion.div)`
-  width:80%;
+  width: 80%;
   border-radius: 1rem;
   padding: 2rem 5rem;
   background: white;
   position: absolute;
   left: 10%;
   color: black;
-  img{
+  img {
     width: 100%;
   }
 `;
@@ -89,30 +123,25 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
 `;
 
 const Info = styled(motion.div)`
-  text-align:center;
-
+  text-align: center;
 `;
 
 const Platforms = styled(motion.div)`
-
   display: flex;
   justify-content: space-evenly;
-  img{
+  img {
     margin-left: 3rem;
   }
 `;
 
 const Media = styled(motion.div)`
-  margin-top:5rem;
-  img{
-    width:100%;
-    
+  margin-top: 5rem;
+  img {
+    width: 100%;
   }
-
 `;
 
 const Description = styled(motion.div)`
